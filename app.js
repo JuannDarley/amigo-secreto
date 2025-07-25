24bcce8e815a7497accb420093c56eceb3dc0fd6
Juan Darley <juandarley99@gmail.com>
2025-07-25 04:14:48 +0200

    Realizando Sorteio e retirando nomes sorteados da lista

--- app.js
+++ app.js
@@ -9,10 +9,11 @@ function adicionarAmigo() {
             return alert('Este nome já está na lista.');
         } else {
             listaDeNomes.push(nome);
-           let exibirLista = document.getElementById('listaAmigos');
+            let exibirLista = document.getElementById('listaAmigos');
            let nomesNaLista = listaDeNomes.map (nome => `<li>${nome}</li>`).join('');
            exibirLista.innerHTML = nomesNaLista;
-           limparLista();
+           limparCampo();
+          
         }      
         
     } else {
@@ -21,7 +22,35 @@ function adicionarAmigo() {
    
 }
 
-function limparLista() {
+function limparCampo() {
     document.querySelector('input').value = '';
     document.querySelector('input').focus();
 }
+
+function limparLista() {
+    listaDeNomes = [];
+    let exibirLista = document.getElementById('listaAmigos');
+    exibirLista.innerHTML = '';
+}
+
+function sortearAmigo() {
+    if (listaDeNomes.length === 0) {
+        return alert('A lista de amigos está vazia. Adicione amigos antes de sortear.');
+    }
+    
+    let indiceAleatorio = Math.floor(Math.random() * listaDeNomes.length);
+    let amigoSorteado = listaDeNomes[indiceAleatorio];
+    listaDeNomes[indiceAleatorio] = listaDeNomes[listaDeNomes.length - 1];
+    listaDeNomes.pop();
+    let exibirLista = document.getElementById('listaAmigos');
+           let nomesNaLista = listaDeNomes.map (nome => `<li>${nome}</li>`).join('');
+           exibirLista.innerHTML = nomesNaLista;
+    
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `O amigo sorteado é: <strong>${amigoSorteado}</strong>`;
+    if(listaDeNomes.length === 0) {
+        limparLista();
+    }
+    
+}
+
